a2617003ea677961c712c82422791d61f4a0957a
shubham-sapkal <sapkal.shubham2002@gmail.com>
2023-06-13 22:58:00 +0530

    Client URL change

--- client/script.js
+++ client/script.js
@@ -87,7 +87,7 @@ const handleSubmit = async (e) => {
 
     // fetch data from server -> bot 
 
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://shubham-divinegpt.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
