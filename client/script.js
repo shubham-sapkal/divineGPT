29c5bafa59587afaa5504d92ad19dd21a3c76b95
shubham-sapkal <sapkal.shubham2002@gmail.com>
2023-06-14 12:27:16 +0530

    icon error

--- client/script.js
+++ client/script.js
@@ -1,5 +1,5 @@
-const bot = "./assets/bot.svg";
-const user = "./assets/user.svg";
+import bot from "./assets/bot.svg";
+import user from "./assets/user.svg";
 
 const form = document.querySelector("form");
 const chatContainer = document.querySelector("#chat_container");
